9ea6b6920a44eab6c8690ee26fe69104481ef191
eric <ericmarcelo455@gmail.com>
2023-01-26 16:14:18 -0800

    POJO
    * Adding POJOs to com.example.spc.models

---
+++ spring-pet-clinic/src/main/java/com/example/spc/models/Person.java
@@ -0,0 +1,23 @@
+package com.example.spc.models;
+
+public class Person {
+	
+	private String frstName;
+	private String lastname;
+	
+	public String getFrstName() {
+		return frstName;
+	}
+	public void setFrstName(String frstName) {
+		this.frstName = frstName;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	
+
+}
---
+++ spring-pet-clinic/src/main/java/com/example/spc/models/Vet.java
@@ -0,0 +1,5 @@
+package com.example.spc.models;
+
+public class Vet extends Person {
+
+}
