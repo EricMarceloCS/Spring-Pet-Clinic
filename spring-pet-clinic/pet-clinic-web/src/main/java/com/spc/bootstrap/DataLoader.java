1376140a4d24f3b4da6300f24c63625f99bb0990
eric <ericmarcelo455@gmail.com>
2023-01-31 09:42:56 -0800

    Data Loader
    * Created bootstrap package
    * Created Data Loader Class
    *

---
+++ spring-pet-clinic/pet-clinic-web/src/main/java/com/spc/bootstrap/DataLoader.java
@@ -0,0 +1,62 @@
+package com.spc.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.spc.models.*;
+import com.spc.services.*;
+import com.spc.services.map.*;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+	private final OwnerService ownerSerivce;
+	private final VetService vetService;
+	private final PetService petService;
+	
+	public DataLoader() {
+		super();
+		// TODO Auto-generated constructor stub
+		this.ownerSerivce = new OwnerServiceMap();
+		this.vetService = new VetServiceMap();
+		this.petService = new PetServiceMap();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Owner owner1 = new Owner();
+		owner1.setId(1L);
+		owner1.setFrstName("Michael");
+		owner1.setLastname("Weston");
+		
+		this.ownerSerivce.save(owner1.getId(), owner1);
+		
+		Owner owner2 = new Owner();
+		owner2.setId(2L);
+		owner2.setFrstName("Fiona");
+		owner2.setLastname("Glenanne");
+		
+		this.ownerSerivce.save(owner2.getId(), owner2);
+		
+		System.out.println("Loaded Owners...");
+		
+		Vet vet1 = new Vet();
+		vet1.setId(1L);
+		vet1.setFrstName("Sam");
+		vet1.setLastname("Axe");
+		
+		this.vetService.save(vet1.getId(), vet1);
+		
+		Vet vet2 = new Vet();
+		vet2.setId(2L);
+		vet2.setFrstName("Jessie");
+		vet2.setLastname("Porter");
+		
+		this.vetService.save(vet2.getId(), vet2);
+		
+		System.out.println("Loaded Vets...");
+
+	}
+
+}
