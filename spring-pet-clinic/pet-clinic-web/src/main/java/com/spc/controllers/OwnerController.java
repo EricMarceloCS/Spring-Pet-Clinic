a51520bea8919c810f32b724461669373b1ffa28
eric <ericmarcelo455@gmail.com>
2023-02-01 11:15:10 -0800

    Web Resource Optimizer
    * Added WRO to Pom File
    * Added less files to src/main/less
    * Added wro file to src/main/wro

--- spring-pet-clinic/pet-clinic-web/src/main/java/com/spc/controllers/OwnerController.java
+++ spring-pet-clinic/pet-clinic-web/src/main/java/com/spc/controllers/OwnerController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.spc.services.OwnerService;
+import com.spc.services.*;
 
 @Controller
 @RequestMapping("/owners")
