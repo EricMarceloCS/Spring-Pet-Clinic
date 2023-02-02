f745a65c597b487881253005b47031ca8a4f6032
eric <ericmarcelo455@gmail.com>
2023-02-01 17:34:11 -0800

    Enhancements
    * modified save of OwnerService map to save owner's pets pet type and
      pet id
    * instantiated Sets
    * loaded pet data
    * created pets
    * assigned owners
    * saved pets to owners
    * added owner contact information

---
+++ spring-pet-clinic/pet-clinic-data/src/main/java/com/spc/services/map/PetTypeServiceMap.java
@@ -0,0 +1,42 @@
+package com.spc.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.spc.models.PetType;
+import com.spc.services.PetTypeService;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+	
+	@Override
+	public Set<PetType> findAll(){
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType object) {
+		// TODO Auto-generated method stub
+		return super.save(object);
+	}
+
+	@Override
+	public void delete(PetType object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+}
--- spring-pet-clinic/pet-clinic-web/src/main/java/com/spc/bootstrap/DataLoader.java
+++ spring-pet-clinic/pet-clinic-web/src/main/java/com/spc/bootstrap/DataLoader.java
@@ -1,5 +1,9 @@
 package com.spc.bootstrap;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,26 +17,59 @@ public class DataLoader implements CommandLineRunner {
 
 	private final OwnerService ownerSerivce;
 	private final VetService vetService;
-	
-	@Autowired
-	public DataLoader(OwnerService ownerSerivce, VetService vetService) {
+	private final PetTypeService petTypeService;
+
+	@Autowired //included for clarity
+	public DataLoader(OwnerService ownerSerivce, VetService vetService, PetTypeService petTypeService) {
 		super();
 		this.ownerSerivce = ownerSerivce;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		
+		PetType savedDogPetType = this.petTypeService.save(dog);
+		
+		PetType cat = new PetType();
+		cat.setName("Cat");
+		
+		PetType savedCatType = this.petTypeService.save(cat);
+		
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Michael");
 		owner1.setLastName("Weston");
+		owner1.setAddress("123 Brickerel");
+		owner1.setCity("Miami");
+		owner1.setTelephone("1231234567");
 		
+		Pet mikesPet = new Pet();
+		mikesPet.setPetType(savedDogPetType);
+		mikesPet.setOwner(owner1);
+		mikesPet.setBirthDate(LocalDate.now());
+		mikesPet.setName("Roscoe");
+		
+		owner1.getPets().add(mikesPet);
 		this.ownerSerivce.save(owner1);
 		
 		Owner owner2 = new Owner();
 		owner2.setFirstName("Fiona");
 		owner2.setLastName("Glenanne");
+		owner2.setAddress("123 Brickerel");
+		owner2.setCity("Miami");
+		owner2.setTelephone("1231234567");
+		
+		Pet fionasCat = new Pet();
+		fionasCat.setPetType(savedCatType);
+		fionasCat.setOwner(owner2);
+		fionasCat.setBirthDate(LocalDate.now());
+		fionasCat.setName("Just Cat");
+		owner2.getPets().add(fionasCat);
 		
 		this.ownerSerivce.save(owner2);
 		
