cc5d24a91e450de684efb0434bdd50cb0c0b69dc
eric <ericmarcelo455@gmail.com>
2023-02-07 08:48:48 -0800

    Lombok
    * Refactored to use Project Lombok
    * Created Builder Pattern for Owner

--- spring-pet-clinic/pet-clinic-data/src/main/java/com/spc/models/BaseEntity.java
+++ spring-pet-clinic/pet-clinic-data/src/main/java/com/spc/models/BaseEntity.java
@@ -6,7 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 	
@@ -15,13 +23,5 @@ public class BaseEntity implements Serializable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
 
 }
--- spring-pet-clinic/pet-clinic-data/src/main/java/com/spc/models/Visit.java
+++ spring-pet-clinic/pet-clinic-data/src/main/java/com/spc/models/Visit.java
@@ -1,8 +1,20 @@
 package com.spc.models;
 
 import java.time.LocalDate;
-import jakarta.persistence.*;
+import java.util.Set;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name="visits")
 public class Visit extends BaseEntity {
@@ -19,24 +31,5 @@ public class Visit extends BaseEntity {
 	@JoinColumn(name="pet_id")
 	private Pet pet;
 	
-	public LocalDate getDate() {
-		return date;
-	}
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public Pet getPet() {
-		return pet;
-	}
-	public void setPet(Pet pet) {
-		this.pet = pet;
-	}
 
-	
 }
